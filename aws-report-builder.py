31ce5ec2030c8783e3ea7de1a4cfd949ba044a34
Raghul Christus <raghul.christus@gmail.com>
2020-04-24 18:33:39 +0530

    Add uptime functionality

--- aws-report-builder.py
+++ aws-report-builder.py
@@ -32,7 +32,6 @@ def print_running_instance_from_region(region='ap-south-1'):
        # Printing to Table
        table = convert_to_table(["instance_id", "instance_name", "instance_type", "instance_state", "instance_uptime", "instance_vpc_id", "instance_az"], data)
        # Print to HTML
-       #ret_str = ret_str + "<h2> Instances from Region: "+region+" </h2>"
        ret_str = ret_str + "<h2> Region: "+region+" </h2>"
        ret_str = ret_str + table.get_html_string()
    else:
@@ -66,7 +65,10 @@ def convert_to_table(heading, values):
 def get_newly_launched_instances(region):
    pass
 
-def get_running_instances_from_region(region):
+def get_running_instances_for_last_7_days(region):
+   pass
+
+def get_running_instances_from_region(region, uptime=0):
     # Return Data Structure - List of Dictionaries - [{instance1}, {instance2}]
    data = []
    ec2 = boto3.client('ec2', region_name=region)
@@ -78,14 +80,21 @@ def get_running_instances_from_region(region):
                  }
              ]
              )
+
    for reservation in res['Reservations']:
        for instance in reservation['Instances']:
            instance_data = {}
+           append = True
+           # Convert Launch time to days
+           instance_data['instance_uptime'] = calculate_instance_age(instance['LaunchTime'])
+           if ((uptime != 0) and (not instance_data['instance_uptime'] <= uptime)):
+               append = False
+               continue;
+           else:
+               instance_data['instance_uptime'] = str(calculate_instance_age(instance['LaunchTime']))
            instance_data['instance_id'] = instance['InstanceId']
            instance_data['instance_type'] = instance['InstanceType']
            instance_data['instance_state'] = instance['State']['Name']
-           # Convert Launch time to days
-           instance_data['instance_uptime'] = str(calculate_instance_age(instance['LaunchTime']))
            instance_data['instance_vpc_id'] = instance['VpcId']
            instance_data['instance_az'] = instance['Placement']['AvailabilityZone']
            instance_data['instance_name'] = ""
@@ -96,18 +105,47 @@ def get_running_instances_from_region(region):
            except:
                #Either no TAG or no instance itself
                pass
-       data.append(instance_data)
+       if append:
+           data.append(instance_data)
 
    return (data)
+
+@app.route("/ec2")
+def get_ec2():
+    # Parsing the Query String
+    region = request.args.get('region', "all")
+    state = request.args.get('state', 'running')
+    uptime = int(request.args.get('uptime', '0'))
+
+    if region == "all":
+        all_regions = get_all_regions()
+    else: 
+        all_regions = region
+
+    ret_str = ""
+    for reg in all_regions:
+        data = get_running_instances_from_region(reg, uptime)
+
+        if len(data) >= 1:
+            # Printing to Table
+            table = convert_to_table(["instance_id", "instance_name", "instance_type", "instance_state", "instance_uptime", "instance_vpc_id", "instance_az"], data)
+            # Print to HTML
+            ret_str = ret_str + "<h2> Region: "+reg+" </h2>"
+            ret_str = ret_str + table.get_html_string()
+        else:
+            ret_str = ret_str + "<h2> Region: "+reg+" </h2><h4> No Instances in Running State in this Region </h4>"
+
+    return ret_str
+
 
 def calculate_instance_age(launch_time):
 
     my_time_zone = pytz.timezone('Asia/Kolkata')
     current_time = my_time_zone.localize(datetime.datetime.now())
     
-    diff = launch_time - current_time
+    diff = current_time - launch_time
     # This returns days in int
-    # Example: -249 days
+    # Example: 249 days
     return diff.days
 
 if __name__ == '__main__':
